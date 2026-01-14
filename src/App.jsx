22ac481049f22ed868480183cbbb595036cc1692
Sharleen10 <sharleensshabangu@gmail.com>
2026-01-14 14:18:46 +0200

    Updated Routing to next pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Home from './components/Hero';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Hero />
-      {/* Additional sections will go here */}
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
